3c0e98c0cc5e413fa0f615efdf42ff5e1bbd2279
Artem Kuznetsov <artemkuznetsov.03@mail.ru>
2024-01-28 05:03:32 +0500

    add new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def raise_letters(text=input()):
+    """Возвращает строку заглавными буквами"""
     return text.upper()
+
+
+def title_letters(text=input()):
+    """Возвращает строку с первыми заглавными буквами"""
+    return text.title()
